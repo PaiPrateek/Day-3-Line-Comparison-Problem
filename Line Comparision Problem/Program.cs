65544aaa8f50266c51ea402d0a0bbf9df9485f5a
Prateek Pai <paiprateek123@gmail.com>
2022-06-10 22:35:48 +0530

    Welcome to Line Comparision Computation Problem

---
+++ Line Comparision Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparision_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+        }
+    }
+}
